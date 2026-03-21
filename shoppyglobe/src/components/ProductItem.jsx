9c49c5f2302664a957d9b383eb905d0440d8a3ff
kamalesh <kamalesh6196@gmail.com>
2026-03-22 05:10:30 +0530

    Added productItem component to display details of the product

--- shoppyglobe/src/components/ProductItem.jsx
+++ shoppyglobe/src/components/ProductItem.jsx
@@ -1,6 +1,30 @@
-function ProductItem() {
+import { useDispatch } from "react-redux"
+
+import {Link} from "react-router-dom"
+import { addItem } from "../redux/cartSlice"
+
+function ProductItem({product}) {
+  
+    const dispatch = useDispatch()
   return (
-    <div>ProductItem</div>
+    <div>
+        {/* dispalying product image */}
+        <img src={product.thumbnail}  width ="150" />
+
+        {/* dispalying product title*/}
+        <h3>{product.title}</h3>
+
+        {/* dispalying product price*/}
+         <p>₹ {product.price}</p>
+
+         {/* Link to view Details about the product */}
+         <Link to={"/product/"+product.id}>View Details</Link><br />
+
+         {/* button to add items to the cart */}
+         <button onClick={()=>dispatch(addItem(product))}> Add to Cart</button>
+         
+
+    </div>
   )
 }
 
