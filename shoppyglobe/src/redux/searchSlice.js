00f8e1522b137ef60227084b0cd8d045dbec0dc1
kamalesh <kamalesh6196@gmail.com>
2026-03-22 03:10:41 +0530

    Added search Slice component successfully

---
+++ shoppyglobe/src/redux/searchSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const searchSlice = createSlice({
+    name : "search",
+    initialState : {
+        text : ""
+    },
+    reducers : {
+        setSearch :(state, action)=>{
+            state.text = action.payload
+        }
+    }
+})
+
+export const {setSearch} = searchSlice.actions
+
+export default searchSlice.reducer
