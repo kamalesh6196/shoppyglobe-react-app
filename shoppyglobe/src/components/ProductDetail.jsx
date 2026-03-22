36b92f2c7f4ca41420508778ccebd362d44026cb
kamalesh <kamalesh6196@gmail.com>
2026-03-22 14:59:15 +0530

    Added ProductDetail component to display product details

--- shoppyglobe/src/components/ProductDetail.jsx
+++ shoppyglobe/src/components/ProductDetail.jsx
@@ -1,6 +1,31 @@
+import { useParams } from "react-router-dom"
+import useProductDetail from "../hooks/useProductDetail"
+import { useDispatch } from "react-redux"
+import { addItem } from "../redux/cartSlice"
+
 function ProductDetail() {
+    // fetching id from the url using useparams hook
+    const {id} = useParams()
+    // fetching product based on the id using useProductDetail hook
+    const product = useProductDetail(id)
+    //  using useDispatch hook for dipatching the action
+    const dispatch = useDispatch()
+    //  acts as a fallback source if there is a delay in loading
+    if(!product) return <h2>Loading...</h2>
   return (
-    <div>ProductDetail</div>
+        //  description about the product with it's image and details 
+    <div>
+        <img src={product.thumbnail} width="200" />
+
+        <h2>{product.title}</h2>
+
+        <p>{product.description}</p>
+
+        <h3>₹ {product.price}</h3>
+        {/* dispatching an action on button click to add component to the cart */}
+        <button onClick={()=>dispatch(addItem(product))}>Add to Cart</button>
+
+    </div>
   )
 }
 
