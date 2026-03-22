4ba9a361ce1f56c9cd4e779344c82aa6ddfb55df
kamalesh <kamalesh6196@gmail.com>
2026-03-22 14:46:25 +0530

    implemented custom useProductDetail hook to fetch product data based on the id provided

---
+++ shoppyglobe/src/hooks/useProductDetail.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function useProductDetail(id){
+    const [product , setProduct] = useState(null)
+
+    useEffect(()=>{
+        // function to fetch data based on id
+        async function fetchProduct() {
+            const res = await fetch( `https://dummyjson.com/products/${id}`)
+
+            const data = await res.json()
+            //  setting up the product fetched to useState variable product 
+            setProduct(data)
+        }
+        fetchProduct()
+
+        return product
+    },[id])
+
+}
+
+export default useProductDetail
