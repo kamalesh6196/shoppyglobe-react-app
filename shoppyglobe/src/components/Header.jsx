7535e6d7b9065b3b74b063222ec71ae78e7fbaa7
kamalesh <kamalesh6196@gmail.com>
2026-03-22 17:03:12 +0530

    styled header component

--- shoppyglobe/src/components/Header.jsx
+++ shoppyglobe/src/components/Header.jsx
@@ -1,16 +1,19 @@
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import "./Header.css"
 
 function Header() {
 
     const cartItems = useSelector(store => store.cart.items)
   return (
-    <div>
+    <div className="header">
         <h2>ShoppyGlobe</h2>
+        <div className="nav-links">
+           <Link to="/">Home</Link>
 
-        <Link to="/">Home</Link>
-
-        <Link to="/cart">Cart ({cartItems.length})</Link>
+           <Link to="/cart">Cart ({cartItems.length})</Link>
+        </div>
+        
     </div>
   )
 }
