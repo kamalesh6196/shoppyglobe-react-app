3724c7b1d624ad313c0c5d13aed4417e938b3d99
kamalesh <kamalesh6196@gmail.com>
2026-03-22 05:41:15 +0530

    provided routing in App.jsx with lazy loading and suspense for faster loading

--- shoppyglobe/src/App.jsx
+++ shoppyglobe/src/App.jsx
@@ -1,6 +1,23 @@
+import { lazy, Suspense } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+// Loading the Asynchronous tasks lazily for the faster loading
+const ProductList  = lazy(()=>import("./components/ProductList"))
+
+// creating router using createBrowserRouter for providing routing based on the path 
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <ProductList/>
+  }
+])
 function App() {
   return (
-    <div>App</div>
+    // providing suspense with fallback to all the components inside router
+    <Suspense fallback={<h1>Loading...</h1>}>
+      {/* providing router created using RouterProvider */}
+      <RouterProvider router={router} />
+    </Suspense>
   )
 }
 
