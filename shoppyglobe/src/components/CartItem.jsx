579a9d13323c7846da0e22f576e436e53ae823f3
kamalesh <kamalesh6196@gmail.com>
2026-03-22 15:42:41 +0530

    Added cartItem component to display each item in the cart with it's details and some necessary buttons

--- shoppyglobe/src/components/CartItem.jsx
+++ shoppyglobe/src/components/CartItem.jsx
@@ -1,6 +1,20 @@
-function CartItem() {
+import { useDispatch } from "react-redux"
+import { addItem, removeItem } from "../redux/cartSlice"
+
+function CartItem({item}) {
+    
+    const dispatch = useDispatch()
   return (
-    <div>CartItem</div>
+    <div>
+        {/* Diaplaying each item in the cart with it's product details */}
+        <h3>{item.title}</h3>
+
+        <p>Quantity :{item.quantity}</p>
+        {/* button to increase the quantity of the item added to cart*/}
+        <button onClick={()=> dispatch(addItem(item))}>+</button>
+         {/* button to remove item from the cart */}
+        <button onClick={()=> dispatch(removeItem(item.id))}>remove</button>
+    </div>
   )
 }
 
